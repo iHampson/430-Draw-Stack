57d5c061063564974c79a36f0e348e5b5d5e277e
Ian Hampson <inhampson@gmail.com>
2016-02-22 20:26:45 -0500

    Basic socketIo works and express routing.

--- client/client.js
+++ client/client.js
@@ -1,1 +1,18 @@
 var socket;
+
+/// Connects to the socketIo lib
+function setupSocket(){
+  socket.emit('join'); // Might not need this if i don't need rooms
+
+  socket.on('msg', function(data){
+    console.log(data);
+  });
+
+}
+
+function init(){
+  socket = io.connect();
+  setupSocket();
+}
+
+init();
--- server/server.js
+++ server/server.js
@@ -1,3 +1,42 @@
 var app = require('express')();
 var server = require('http').Server(app);
-var io = require('socketio')(server);
+var io = require('socket.io')(server);
+var fs = require('fs');
+
+var port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+var index = fs.readFileSync(__dirname + "/../client/client.html");
+
+server.listen(port);
+console.log("Listening on 127.0.0.1:" + port);
+
+var onJoin = function(socket){
+    socket.on('join', function(data){
+      socket.emit('msg', {'msg':'joined'});
+      console.log("sent msg packet");
+    });
+};
+
+var onMsg = function(socket){
+
+};
+
+app.get('/', function(req, res){
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.write(index);
+  res.end();
+});
+
+app.get('/client/client.js', function(req,res){
+  res.writeHead(200, {"Content-Type": "application/javascript"});
+  res.write(fs.readFileSync(__dirname+"/../client/client.js"));
+  res.end();
+});
+
+/// Set up event listeners for the socket
+io.sockets.on("connection", function(socket){
+  onJoin(socket);
+  onMsg(socket);
+});
+
+console.log("Server File Running");
